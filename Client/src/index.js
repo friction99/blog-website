af538add264421465f4aff34087ac03453a37d94
Aman <amankun77@gmail.com>
2024-02-04 20:33:21 +0530

    add redux persist

--- Client/src/index.js
+++ Client/src/index.js
@@ -5,12 +5,14 @@ import App from './App';
 import {store} from './redux/store';
 import {Provider} from 'react-redux';
 import reportWebVitals from './reportWebVitals';
-
+import { PersistGate } from 'redux-persist/integration/react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-       <App />
-  </Provider>
+  <PersistGate>
+      <Provider store={store}>
+         <App />
+      </Provider>
+  </PersistGate>
 );
 
 // If you want to start measuring performance in your app, pass a function
